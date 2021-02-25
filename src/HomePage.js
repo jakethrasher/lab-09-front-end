cf0472fa90ebcfc9a4c10dcf99a8ee08f3379dba
Jake Thrasher <mjakethrasher@gmail.com>
2021-02-24 23:41:04 -0800

    style homepage lol

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,11 +4,12 @@ export default class HomePage extends Component {
     render() {
         const homePage={
             textAlign:'center',
-            fontSize:'6em'
+            fontSize:'3em',
+            backgroundColor:'yellow',
         }
         return (
             <div style={ homePage }>
-                <h1>HOMEPAGE</h1>
+                <h1>CAUTION: HOMEPAGE</h1>
             </div>
         )
     }
